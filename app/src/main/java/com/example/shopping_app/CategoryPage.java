ca8a947ac481040919537f347679d3ca8e1652ce
ayberkanayurtdan <ayberkanayurtdan@posta.mu.edu.tr>
2024-01-09 16:36:08 +0300

    comment page added

--- app/src/main/java/com/example/shopping_app/CategoryPage.java
+++ app/src/main/java/com/example/shopping_app/CategoryPage.java
@@ -3,12 +3,26 @@ package com.example.shopping_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class CategoryPage extends AppCompatActivity {
+    private static TextView categoryName;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_page);
+        categoryName = findViewById(R.id.CategoryName);
     }
+    public static void updateCategoryName(String categoryName_) {
+        Log.d("CategoryPageActivity", "setCategoryName called with categoryName: " + categoryName_);
+        // CategoryName'ı değiştir
+        if (categoryName != null) {
+            categoryName.setText(categoryName_);
+        }
+    }
+
 }
--- app/src/main/java/com/example/shopping_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/shopping_app/ui/home/HomeFragment.java
@@ -15,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.shopping_app.CategoryPage;
+import com.example.shopping_app.Comment;
 import com.example.shopping_app.R;
 import com.example.shopping_app.databinding.FragmentHomeBinding;
 
@@ -50,9 +51,12 @@ public class HomeFragment extends Fragment {
         btnMen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Men butonuna basıldığında CategoryPage'e geçiş yap
+                CategoryPage.updateCategoryName("Men");
+
+
                 startActivity(new Intent(getActivity(), CategoryPage.class));
             }
+
         });
 
 
@@ -60,7 +64,7 @@ public class HomeFragment extends Fragment {
         btnWomen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Women butonuna basıldığında CategoryPage'e geçiş yap
+                CategoryPage.updateCategoryName("Women");
                 startActivity(new Intent(getActivity(), CategoryPage.class));
             }
         });
@@ -71,7 +75,8 @@ public class HomeFragment extends Fragment {
         btnUnisex.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Unisex butonuna basıldığında CategoryPage'e geçiş yap
+
+                CategoryPage.updateCategoryName("Unisex");
                 startActivity(new Intent(getActivity(), CategoryPage.class));
             }
         });
@@ -81,7 +86,7 @@ public class HomeFragment extends Fragment {
         btnBaby.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Baby butonuna basıldığında CategoryPage'e geçiş yap
+                CategoryPage.updateCategoryName("Baby");
                 startActivity(new Intent(getActivity(), CategoryPage.class));
             }
         });
@@ -99,13 +104,25 @@ public class HomeFragment extends Fragment {
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // SearchView'dan metni al
+
+                //Comments Sayfasını kontrol etmek için. SİLİNECEK!!!
+                btnSearch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startComment();
+                        System.out.println("Comments");
+                    }
+                });
+                //Comments Sayfasını kontrol etmek için. SİLİNECEK!!!
+
+
+ /*               // SearchView'dan metni al
                 String searchText = searchView.getQuery().toString();
 
 
                 // Alınan metni kullanarak arama işlemini başlat
                 performSearch(searchText);
-            }
+*/            }
         });
 
         return root;
@@ -135,6 +152,9 @@ public class HomeFragment extends Fragment {
         startActivity(new Intent(getActivity(), CategoryPage.class));
     }
 
+    private void startComment() {
+        startActivity(new Intent(getActivity(), Comment.class));
+    }
 
 
 
